c9e9b93aa9ff048bc9d1c118c74bba483379d443
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-12-03 12:28:40 -0800

    Password match JS

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-// GOOGLE DEVKIT DOESN't recognise the JS..
 
-
-// Get references to the password and retypePassword input elements
 const passwordInput = document.getElementById("password");
 const retypePasswordInput = document.getElementById("retypePassword");
 const passwordMatchMessage = document.getElementById("passwordMatchMessage");
 
-// Add an event listener to the retypePassword input
+let timeoutId;
+
 retypePasswordInput.addEventListener("input", function () {
-    // Check if the passwords match
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(function () {
+
     const passwordsMatch = passwordInput.value === retypePasswordInput.value;
 
-    // Display a message based on the comparison result
     passwordMatchMessage.textContent = passwordsMatch ? "Passwords match" : "Passwords do not match";
+
+    passwordMatchMessage.style.color = passwordsMatch ? "green" : "red";
+
+    if (retypePasswordInput.value === "") {
+        passwordMatchMessage.textContent = "";
+    }
+    }, 1000);
 });
